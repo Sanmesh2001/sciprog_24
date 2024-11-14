c1e647fd21f89d812547a77f5cf632163afc6cd8
Sanmesh18 <sanmeshshintre@gmail.com>
2024-11-14 19:57:21 +0000

    Update sum.c

--- practical02/sum.c
+++ practical02/sum.c
@@ -10,14 +10,14 @@ int main(void) {
 /* First sum */
    sum1 = 0.0;
    for (i=1; i<=1000; i++) {
-      sum1+= i
+      sum1 = sum1 + 1.0/(float)i;
    }
 
 
 /* Second sum */
    sum2 = 0.0;
    for (i=1000; i>0; i--) {
-      sum2+= i
+      sum2 = sum2 + 1.0/(float)i;
    }
 
    printf(" Sum1=%f\n",sum1);
